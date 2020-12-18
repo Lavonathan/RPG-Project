e1e5bf27f200bc082059d99cf2063578cdd51108
Lavon <Lavon_Edwards@hotmail.com>
2020-12-17 22:47:46 -0600

    Added NavMesh and Basic Movement
    Learned about “Align with View” option from the gameObject tab, which allows you to move an object to wherever your scene view currently is. This is super handy for moving the camera. Added a navigation mesh, allowing the player to move to places in the world. Completed the challenge on my own! Getting access to a component added on to an object through script, and researched the documentation to fin which namespace was needed.

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+//  Handles the movement of the player game object.
+public class Mover : MonoBehaviour
+{
+    //  The target that we will be moving to.
+    [SerializeField]
+    Transform target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //  Move the navMeshAgent destination property to the position of the target each frame.
+        GetComponent<NavMeshAgent>().destination = target.position; 
+    }
+}
